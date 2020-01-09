0a7f45e188ed82317ebd1cfb3edf013643759f82
sblendo00 <chri.sble@libero.it>
2020-01-09 08:28:46 +0000

    errori risolti e app finita

--- Lego/routes/api3.js
+++ Lego/routes/api3.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+const lego3 = require('../lego.json');
 /* GET home page. */
-router.get('/NomiLego', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/Lego/Name', (req,res) => {  
+    let nomi = [];  //creare un vettore img
+    let vett = Object.keys(lego3.castelli); // 
+    // usiamo vett_people.length perchè il json non ha una misura
+    for(let i = 0; i < vett.length; i++){ 
+        //inseriamo dentro il vettore tutte le image del json in ordine 
+        nomi.push(lego3.castelli[i].Name);            
+    }
+    // dentro data mettiamo img e la farà vedere
+    let data = JSON.stringify(nomi);
+    res.send(data);
 });
 
 module.exports = router;
--- Lego/routes/istruzioni.js
+++ Lego/routes/istruzioni.js
@@ -1,17 +1,17 @@
 var createError = require ('http-errors'); // per creare l'errore
 var express = require('express');
 var router = express.Router();
-var lego = require('../lego.json');
+var lego_file = require('../lego.json');
 // come se ci fosse davanti dettagli
 router.get('/:Number/', function(req,res,next){
-    let lego = lego.castelli.find(p => p.Number == req.params.Number) //poeta contiene la corrispondenza dei 2 parametri: controlla se il numero nell'URL è uguale ad un numero dentro il json
+    let lego = lego_file.castelli.find(p => p.Number == req.params.Number) //poeta contiene la corrispondenza dei 2 parametri: controlla se il numero nell'URL è uguale ad un numero dentro il json
     if (typeof lego === "undefined"){
         return next (createError(300, 'lego non trovato'));
     }
     else{
         res.render ('istruzioni',{
             title : `${lego.Name}`,
-            immagine: `${lego.istruzioni}`, // contiene tutto il contenuto del'json di quel numero specifico inserito nell' URL
+            lego, // contiene tutto il contenuto del'json di quel numero specifico inserito nell' URL
         }) ; ;
     }
 })
